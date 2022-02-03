bba7f823168b87177eeff0f6b6d4166106da71be
Harsh Makwana <harsh.make1998@gmail.com>
2022-02-04 01:18:50 +0530

    Initial commit

---
+++ src/app.controller.ts
@@ -0,0 +1,24 @@
+import { Controller } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { JwtPayload } from 'jsonwebtoken';
+import { AppService } from './app.service';
+import { IDecodeResponse } from './core/interfaces/IDecodeResponse';
+import { ITokenResponse } from './core/interfaces/ITokenResponse';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @MessagePattern('token_create')
+  public async createToken(@Payload() data: string): Promise<ITokenResponse> {
+    const user = JSON.parse(data);
+    return this.appService.createToken(user.id);
+  }
+
+  @MessagePattern('token_decode')
+  public async decodeToken(
+    @Payload() data: string,
+  ): Promise<string | JwtPayload | IDecodeResponse> {
+    return this.appService.decodeToken(data);
+  }
+}
---
+++ src/config/config.service.ts
@@ -0,0 +1,26 @@
+import { config } from 'dotenv';
+import { join } from 'path';
+let envPath;
+if (process.env.NODE_ENV) {
+  envPath = join(__dirname, `../../.${process.env.NODE_ENV}.env`);
+} else {
+  envPath = join(__dirname, '../../../.env');
+}
+config({ path: envPath });
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class ConfigService {
+  private config: { [key: string]: any } = {};
+  constructor() {
+    this.config.accessExp = process.env.ACCESS_EXP;
+    this.config.refreshExp = process.env.REFRESH_EXP;
+    this.config.secretKey = process.env.APP_SECRET;
+    this.config.rb_url = process.env.RABBITMQ_URL;
+    this.config.token_queue = process.env.RABBITMQ_TOKEN_QUEUE;
+  }
+
+  public get(key: string): any {
+    return this.config[key];
+  }
+}
