4c16e1f66e4966590582adccde87a7a2efc86923
alishasimform <alisha.t@simformsolutions.com>
2022-03-03 19:35:56 +0530

    modify: token encoding/decoding payload

--- src/app.service.ts
+++ src/app.service.ts
@@ -8,12 +8,15 @@ import { IDecodeResponse } from './core/interfaces/IDecodeResponse';
 export class AppService {
   constructor(private configService: ConfigService) {}
 
-  public createToken(userId: number): ITokenResponse {
+  public createToken(payload: {
+    userId: number;
+    role: string;
+  }): ITokenResponse {
     const accessExp = this.configService.get('accessExp');
     const refreshExp = this.configService.get('refreshExp');
     const secretKey = this.configService.get('secretKey');
-    const accessToken = sign({ userId }, secretKey, { expiresIn: accessExp });
-    const refreshToken = sign({ userId }, secretKey, { expiresIn: refreshExp });
+    const accessToken = sign(payload, secretKey, { expiresIn: accessExp });
+    const refreshToken = sign(payload, secretKey, { expiresIn: refreshExp });
     return {
       accessToken,
       refreshToken,
